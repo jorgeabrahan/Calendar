2bc0a47e0d369c923a9b699fbb7b4aa12516316f
jorgeabrahan <jorge24abrahan@gmail.com>
2023-09-07 13:37:00 -0600

    Create calendar component

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,47 @@
+import { Calendar } from 'react-big-calendar'
+import { addHours } from 'date-fns'
+import { CalendarEvent, Navbar } from "../";
+import { getMessagesEs, localizer } from '../../helpers';
+
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '1234',
+    name: 'Jorge'
+  }
+}]
+
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log(event, start, end, isSelected);
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+    return {style}
+  }
   return (
     <>
-      <h1>CalendarPage</h1>
+      <Navbar />
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesEs()}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent
+        }}
+      />
     </>
   );
 }
