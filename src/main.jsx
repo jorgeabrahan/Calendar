4a059ab1f0f793be58c3c63c20d534db786a2ea3
jorgeabrahan <jorge24abrahan@gmail.com>
2023-10-19 00:08:45 -0600

    Import the modal styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import { CalendarApp } from './CalendarApp.jsx'
 import './styles.css'
+import './modal.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
